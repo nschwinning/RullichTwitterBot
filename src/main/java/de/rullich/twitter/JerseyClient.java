088757c89f14ae276d8b5c60342b77b3785a904d
Nils Schwinning <nils.schwinning@paluno.uni-due.de>
2017-03-30 16:49:38 +0200

    added jersey client to read derwesten titles

---
+++ src/main/java/de/rullich/twitter/JerseyClient.java
@@ -0,0 +1,62 @@
+package de.rullich.twitter;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class JerseyClient {
+
+	public String getTweet() {
+
+		try {
+			List<String> titles = new ArrayList<String>();
+
+			Client client = Client.create();
+
+			WebResource webResource = client.resource("http://derwesten.de/staedte/essen/rss");
+
+			ClientResponse response = webResource.accept("application/xml").get(ClientResponse.class);
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+			}
+
+			String output = response.getEntity(String.class);
+			
+			//System.out.println(output);
+
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+		            .parse(new InputSource(new StringReader(output)));
+			NodeList itemNodes = doc.getElementsByTagName("item");
+			for (int i=0; i<itemNodes.getLength();i++){
+				Node node = itemNodes.item(i);
+				NodeList children = node.getChildNodes();
+				for (int j=0;j<children.getLength();j++){
+					if (children.item(j).getNodeName().equals("title")){
+						titles.add(children.item(j).getTextContent());
+					}
+				}
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		return "Blabla";
+
+	}
+
+}
--- src/main/java/de/rullich/twitter/Test.java
+++ src/main/java/de/rullich/twitter/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i=0;i<10;i++)
-			System.out.println(NightTweet.getTweet());
+		JerseyClient jc = new JerseyClient();
+		jc.getTweet();
 		
 	}
 
